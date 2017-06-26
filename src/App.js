098b93ba9496e4516f386f83ce712e91051f9d6b
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-06-26 22:51:22 +0900

    made pad colors into separate objects and refactored the playSound and buttonPress functions, the latter of which was changed to padPress

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 class Simon extends React.Component {
@@ -6,38 +6,52 @@ class Simon extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      green: "inactive",
-      red: "inactive",
-      yellow: "inactive",
-      blue: "inactive",
-      // greenSound: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3",
-      // redSound: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
-      // yellowSound: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
-      // blueSound: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
-      greenFreq: 329.628,
-      redFreq: 220,
-      yellowFreq: 277.183,
-      blueFreq: 164.814,
+      green: {
+        id: "green",
+        status: "inactive",
+        freq: 329.628,
+      },
+      red: {
+        id: "red",
+        status: "inactive",
+        freq: 220,
+      },
+      yellow: {
+        id: "yellow",
+        status: "inactive",
+        freq: 277.183,
+      },
+      blue: {
+        id: "blue",
+        status: "inactive",
+        freq: 164.814,
+      },
       audioContext: new AudioContext(),
       oscillator: null,
       padSequence: [],
-      playerGuesses: []
+      playerGuesses: [],
+      difficulty: 1,
+      numOfSequences: 8,
+      gameOn: false,
     }
-    this.buttonPress = this.buttonPress.bind(this);
+    this.padPress = this.padPress.bind(this)
+    this.setSequences = this.setSequences.bind(this)
   } // end constructor
 
-  buttonPress(buttonColor, buttonSound) {
+  padPress(padObj) {
+    const padKey = padObj.id // makes returning the object in setState much easier
+    const padStatus = padObj.status
+    const padFreq = padObj.freq
 
-    const buttonStatus = this.state[buttonColor]
-    // const buttonAudio = new Audio(this.state[buttonSound])
-
-    // console.log(buttonAudio)
-
+    // return the whole object with all key value pairs -- necessary to prevent errors with the playSound function
     return this.setState({
-      [buttonColor]: buttonStatus === "inactive" ? "active" : "inactive"
+      [padKey]: {
+        status: padStatus  === "inactive" ? "active" : "inactive",
+        id: padObj.id,
+        freq: padFreq
+      }
     })
-    // buttonAudio.play()
-  }
+  } // end padPress
 
   playSound(freq) {
     let audioContext = this.state.audioContext
@@ -52,15 +66,11 @@ class Simon extends React.Component {
     return this.setState({
       oscillator: oscillator,
     })
-
-    // return console.log(freq)
-  }
+  } // end playSound
 
   stopSound() {
     let audioContext = this.state.audioContext
     let oscillator = this.state.oscillator
-
-    console.log(oscillator)
 
     oscillator.stop()
     oscillator.disconnect(audioContext.destination);
@@ -68,21 +78,58 @@ class Simon extends React.Component {
     return this.setState({
       oscillator: null,
     })
+  } // end stopSound
 
-  }
+  setSequences(difficulty) {
+    if (difficulty === 1) {
+      return 8
+    } else if (difficulty === 2) {
+      return 14
+    } else if (difficulty === 3) {
+      return 20
+    } else if (difficulty === 4) {
+      return 31
+    }
+  } // end setSequences
 
-  activateButtons() {
+  toggleOnOff() {
+    const gameOnState = this.state.gameOn
 
-  }
+    return this.setState({
+      gameOn: gameOnState === false ? true : false,
+    })
+  } // end toggleOnOff
+
+  // populatePadSequence() {
+  //   const numOfSequences = this.state.numOfSequences
+  //   const pads = this.state.pads
+  //
+  //
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    const gameWillTurnOn = nextState.gameOn
+
+    if (gameWillTurnOn) {
+      this.populatePadSequence()
+    }
+  } // end componentWillUpdate
 
   render() {
     return (
-      <div className="pads">
-        <div className={`pad green ${this.state.green}`} onMouseDown={() => {this.buttonPress("green"); this.playSound(this.state.greenFreq)}} onMouseUp={() => {this.buttonPress("green"); this.stopSound()}}></div>
-        <div className={`pad red ${this.state.red}`} onMouseDown={() => {this.buttonPress("red"); this.playSound(this.state.redFreq)}} onMouseUp={() => {this.buttonPress("red"); this.stopSound()}}></div>
-        <div className={`pad yellow ${this.state.yellow}`} onMouseDown={() => {this.buttonPress("yellow"); this.playSound(this.state.yellowFreq)}} onMouseUp={() => {this.buttonPress("yellow"); this.stopSound()}}></div>
-        <div className={`pad blue ${this.state.blue}`} onMouseDown={() => {this.buttonPress("blue"); this.playSound(this.state.blueFreq)}} onMouseUp={() => {this.buttonPress("blue"); this.stopSound()}}></div>
-      </div> // end pads
+      <div className="content">
+        <div className="pads">
+          <div className={`pad green ${this.state.green.status}`} onMouseDown={() => {this.padPress(this.state.green); this.playSound(this.state.green.freq)}} onMouseUp={() => {this.padPress(this.state.green); this.stopSound()}}></div>
+          <div className={`pad red ${this.state.red.status}`} onMouseDown={() => {this.padPress(this.state.red); this.playSound(this.state.red.freq)}} onMouseUp={() => {this.padPress(this.state.red); this.stopSound()}}></div>
+          <div className={`pad yellow ${this.state.yellow.status}`} onMouseDown={() => {this.padPress(this.state.yellow); this.playSound(this.state.yellow.freq)}} onMouseUp={() => {this.padPress(this.state.yellow); this.stopSound()}}></div>
+          <div className={`pad blue ${this.state.blue.status}`} onMouseDown={() => {this.padPress(this.state.blue); this.playSound(this.state.blue.freq)}} onMouseUp={() => {this.padPress(this.state.blue); this.stopSound()}}></div>
+        </div>
+        {/* end pads */}
+        <div className="controls">
+          <button type="button" value="start" className="start-button" onClick={() => this.toggleOnOff()}>Start</button>
+        </div>
+        {/* end controls */}
+      </div> // end content
     ); // end return
   } // end render
 } // end Simon Component
