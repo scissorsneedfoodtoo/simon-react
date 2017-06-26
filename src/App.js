32b3e1393be5dbb6db0d6766e38dd0111aa20227
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2017-06-26 18:38:36 +0900

    working button presses and button tones using the Web Audio API, AudioContext, oscillator, and so on

--- src/App.js
+++ src/App.js
@@ -10,20 +10,65 @@ class Simon extends React.Component {
       red: "inactive",
       yellow: "inactive",
       blue: "inactive",
+      // greenSound: "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3",
+      // redSound: "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3",
+      // yellowSound: "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3",
+      // blueSound: "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3",
+      greenFreq: 329.628,
+      redFreq: 220,
+      yellowFreq: 277.183,
+      blueFreq: 164.814,
+      audioContext: new AudioContext(),
+      oscillator: null,
       padSequence: [],
       playerGuesses: []
     }
     this.buttonPress = this.buttonPress.bind(this);
   } // end constructor
 
-  buttonPress(buttonColor) {
+  buttonPress(buttonColor, buttonSound) {
 
     const buttonStatus = this.state[buttonColor]
-    console.log(buttonStatus, buttonColor, this.state)
+    // const buttonAudio = new Audio(this.state[buttonSound])
 
-    this.setState({
+    // console.log(buttonAudio)
+
+    return this.setState({
       [buttonColor]: buttonStatus === "inactive" ? "active" : "inactive"
     })
+    // buttonAudio.play()
+  }
+
+  playSound(freq) {
+    let audioContext = this.state.audioContext
+    let oscillator = this.state.oscillator
+
+    oscillator = audioContext.createOscillator()
+    oscillator.type = 'square'
+    oscillator.connect(audioContext.destination)
+    oscillator.frequency.value = freq
+    oscillator.start()
+
+    return this.setState({
+      oscillator: oscillator,
+    })
+
+    // return console.log(freq)
+  }
+
+  stopSound() {
+    let audioContext = this.state.audioContext
+    let oscillator = this.state.oscillator
+
+    console.log(oscillator)
+
+    oscillator.stop()
+    oscillator.disconnect(audioContext.destination);
+
+    return this.setState({
+      oscillator: null,
+    })
+
   }
 
   activateButtons() {
@@ -33,10 +78,10 @@ class Simon extends React.Component {
   render() {
     return (
       <div className="pads">
-        <div className={`pad green ${this.state.green}`} onMouseDown={() => this.buttonPress("green")} onMouseUp={() => this.buttonPress("green")}></div>
-        <div className={`pad red ${this.state.red}`} onMouseDown={() => this.buttonPress("red")} onMouseUp={() => this.buttonPress("red")}></div>
-        <div className={`pad yellow ${this.state.yellow}`} onMouseDown={() => this.buttonPress("yellow")} onMouseUp={() => this.buttonPress("yellow")}></div>
-        <div className={`pad blue ${this.state.blue}`} onMouseDown={() => this.buttonPress("blue")} onMouseUp={() => this.buttonPress("blue")}></div>
+        <div className={`pad green ${this.state.green}`} onMouseDown={() => {this.buttonPress("green"); this.playSound(this.state.greenFreq)}} onMouseUp={() => {this.buttonPress("green"); this.stopSound()}}></div>
+        <div className={`pad red ${this.state.red}`} onMouseDown={() => {this.buttonPress("red"); this.playSound(this.state.redFreq)}} onMouseUp={() => {this.buttonPress("red"); this.stopSound()}}></div>
+        <div className={`pad yellow ${this.state.yellow}`} onMouseDown={() => {this.buttonPress("yellow"); this.playSound(this.state.yellowFreq)}} onMouseUp={() => {this.buttonPress("yellow"); this.stopSound()}}></div>
+        <div className={`pad blue ${this.state.blue}`} onMouseDown={() => {this.buttonPress("blue"); this.playSound(this.state.blueFreq)}} onMouseUp={() => {this.buttonPress("blue"); this.stopSound()}}></div>
       </div> // end pads
     ); // end return
   } // end render
